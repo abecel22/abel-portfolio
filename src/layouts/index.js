e8884db7cdba974d013388695ff506de99f6d7b7
abecel22 <abe2211@gmail.com>
2018-04-05 22:03:15 -0500

    Added normalize CSS and a SASS pugin.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
-import './index.css'
+import 'normalize.css'
+import './index.scss'
 
 const TemplateWrapper = ({ children }) => (
   <div>
