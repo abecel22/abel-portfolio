39fcffd87dde755fad6a8ce72f3194024a0fb3f2
abecel22 <abe2211@gmail.com>
2018-04-05 21:34:21 -0500

    Removed all default styles and html.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,23 +8,14 @@ import './index.css'
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Abel Acosta"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <div>{children()}</div>
   </div>
 )
 
