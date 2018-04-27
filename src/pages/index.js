48c091d3845bf9650ffd3d0de9a9ac49109c2bcc
abecel22 <abe2211@gmail.com>
2018-04-27 17:01:34 -0500

    Added typewriter animation. Worked on media queries.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,12 @@ import './style.scss'
 const IndexPage = () => (
   <div className="container">
     <div className="left__div">
-      <h2>Hi, I'm Abel Acosta</h2>
+      <p className="typewriter title">Hi, I'm Abel Acosta. I make web apps!</p>
       <h3>
-        Front end web developer. I make web apps! Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Iusto dolore accusantium perferendis
-        ducimus natus aperiam ab, harum, quas nobis veritatis fugit eius atque
-        explicabo, ut quaerat dignissimos similique velit ea provident quo.
+        Front end web developer. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Iusto dolore accusantium perferendis ducimus natus
+        aperiam ab, harum, quas nobis veritatis fugit eius atque explicabo, ut
+        quaerat dignissimos similique velit ea provident quo.
       </h3>
       <h2 className="projects__title">Projects</h2>
       <Projects />
