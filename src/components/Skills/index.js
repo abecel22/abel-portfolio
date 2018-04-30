632b58f3849972d3295fb6d581fb9557cfcdbdf6
abecel22 <abe2211@gmail.com>
2018-04-29 22:39:03 -0500

    Changed Skills component to table w/ icons.

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import brands from '@fortawesome/fontawesome-free-brands'
+import solid from '@fortawesome/fontawesome-free-solid/'
 
 import './style.scss'
 
@@ -9,12 +12,74 @@ class Skills extends React.Component {
         <h2 className="skills">What I Do</h2>
         <div className="skillsDiv">
           <div className="fb">
-            <h4>Front End</h4>
-            <p>JavaScript, CSS3, HTML5, React</p>
+            <table>
+              <tr>
+                <th />
+                <th>Front End</th>
+              </tr>
+              <tr>
+                <td>
+                  <FontAwesomeIcon className="fa-2x" icon={['fab', 'js']} />
+                </td>
+                <td>JavaScript</td>
+              </tr>
+              <tr>
+                <td>
+                  <FontAwesomeIcon className="fa-2x" icon={['fab', 'react']} />
+                </td>
+                <td>React</td>
+              </tr>
+              <tr>
+                <td>
+                  <FontAwesomeIcon className="fa-2x" icon={['fab', 'css3']} />
+                </td>
+                <td>CSS3</td>
+              </tr>
+              <tr>
+                <td>
+                  <FontAwesomeIcon className="fa-2x" icon={['fab', 'html5']} />
+                </td>
+                <td>HTML5</td>
+              </tr>
+            </table>
           </div>
           <div className="fb">
-            <h4>Back End</h4>
-            <p>NodeJS, Express, APIs, MongoDB</p>
+            <table>
+              <tr>
+                <th />
+                <th colspan="2">Back End</th>
+              </tr>
+              <tr>
+                <td>
+                  <FontAwesomeIcon className="fa-2x" icon={['fab', 'node']} />
+                </td>
+                <td>NodeJS</td>
+              </tr>
+              <tr>
+                <td>
+                  <FontAwesomeIcon
+                    className="fa-2x"
+                    icon={['fab', 'node-js']}
+                  />
+                </td>
+                <td>Express</td>
+              </tr>
+              <tr>
+                <td>
+                  <FontAwesomeIcon className="fa-2x" icon={['fas', 'code']} />
+                </td>
+                <td>APIs</td>
+              </tr>
+              <tr>
+                <td>
+                  <FontAwesomeIcon
+                    className="fa-2x"
+                    icon={['fas', 'database']}
+                  />
+                </td>
+                <td>MongoDB</td>
+              </tr>
+            </table>
           </div>
         </div>
       </div>
