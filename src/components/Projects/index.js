cc3f2cb8b8d0f36921b266956eba0a9b69dab8de
abecel22 <abe2211@gmail.com>
2018-05-02 21:27:49 -0500

    Improved media queries and margins.

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
-import campDash from './../../assets/images/projects/CampDash.png'
+import campDash from './../../assets/images/projects/campDash.png'
+import camp from './../../assets/images/projects/camp.png'
+import drums from './../../assets/images/projects/drums.png'
+import gecko from './../../assets/images/projects/gecko.png'
+import quote from './../../assets/images/projects/quote.png'
+import weather from './../../assets/images/projects/weather.png'
 
 import './style.scss'
 
@@ -9,33 +14,33 @@ class Projects extends React.Component {
     return (
       <div className="projects-grid">
         <div>
-          <img src={campDash} className="projects-row__image" />
-          <h4>Project Name</h4>
+          <img src={gecko} className="projects-row__image" />
+          <h4>GeckoFit</h4>
           <p>This is the short summary. Built on React and Express.</p>
         </div>
         <div>
           <img src={campDash} className="projects-row__image" />
-          <h4>Project Name</h4>
+          <h4>CampDash</h4>
           <p>This is the short summary. Built on React and Express.</p>
         </div>
         <div>
-          <img src={campDash} className="projects-row__image" />
-          <h4>Project Name</h4>
+          <img src={camp} className="projects-row__image" />
+          <h4>YelpCamp</h4>
           <p>This is the short summary. Built on React and Express.</p>
         </div>
         <div>
-          <img src={campDash} className="projects-row__image" />
-          <h4>Project Name</h4>
+          <img src={weather} className="projects-row__image" />
+          <h4>Costa Weather</h4>
           <p>This is the short summary. Built on React and Express.</p>
         </div>
         <div>
-          <img src={campDash} className="projects-row__image" />
-          <h4>Project Name</h4>
+          <img src={quote} className="projects-row__image" />
+          <h4>Quote Generator</h4>
           <p>This is the short summary. Built on React and Express.</p>
         </div>
         <div>
-          <img src={campDash} className="projects-row__image" />
-          <h4>Project Name</h4>
+          <img src={drums} className="projects-row__image" />
+          <h4>JS Drum Kit</h4>
           <p>This is the short summary. Built on React and Express.</p>
         </div>
       </div>
