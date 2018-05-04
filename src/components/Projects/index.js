afa508e63bfea8af66b5496bd5bfb1f20296b3b4
abecel22 <abe2211@gmail.com>
2018-05-03 22:39:42 -0500

    Added project details and anchor tags.

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -14,34 +14,53 @@ class Projects extends React.Component {
     return (
       <div className="projects-grid">
         <div>
-          <img src={gecko} className="projects-row__image" />
-          <h4>GeckoFit</h4>
-          <p>This is the short summary. Built on React and Express.</p>
-        </div>
-        <div>
-          <img src={campDash} className="projects-row__image" />
-          <h4>CampDash</h4>
-          <p>This is the short summary. Built on React and Express.</p>
-        </div>
-        <div>
-          <img src={camp} className="projects-row__image" />
+          <a href="http://costa-yelp-camp.herokuapp.com/">
+            <img src={camp} className="projects-row__image" />
+          </a>
           <h4>YelpCamp</h4>
-          <p>This is the short summary. Built on React and Express.</p>
+          <p>
+            Learn about, add, and review campgrounds! JS, Express, and MongoDB.
+          </p>
         </div>
         <div>
-          <img src={weather} className="projects-row__image" />
+          <a href="http://camp-dash.surge.sh/">
+            <img src={campDash} className="projects-row__image" />
+          </a>
+          <h4>CampDash</h4>
+          <p>
+            New tab Chrome Extension clone of Momentum. jQuery, SCSS, and APIs.
+          </p>
+        </div>
+        <div>
+          <a href="http://geckofit.surge.sh/">
+            <img src={gecko} className="projects-row__image" />
+          </a>
+          <h4>GeckoFit</h4>
+          <p>
+            A nutrition planning app built with a remote team! React, ES6, and
+            CSS.
+          </p>
+        </div>
+        <div>
+          <a href="https://costa-weather.surge.sh/">
+            <img src={weather} className="projects-row__image" />
+          </a>
           <h4>Costa Weather</h4>
-          <p>This is the short summary. Built on React and Express.</p>
+          <p>Weather app! Weather Underground API and jQuery.</p>
         </div>
         <div>
-          <img src={quote} className="projects-row__image" />
+          <a href="https://costa-quotes.surge.sh/">
+            <img src={quote} className="projects-row__image" />
+          </a>
           <h4>Quote Generator</h4>
-          <p>This is the short summary. Built on React and Express.</p>
+          <p>Generate an uplifting quote on demand! JS, AJAX, and CSS.</p>
         </div>
         <div>
-          <img src={drums} className="projects-row__image" />
+          <a href="https://js-drumkit-aa.surge.sh/">
+            <img src={drums} className="projects-row__image" />
+          </a>
           <h4>JS Drum Kit</h4>
-          <p>This is the short summary. Built on React and Express.</p>
+          <p>Jam out on this online Drum Kit! Vanilla JS and ES6.</p>
         </div>
       </div>
     )
