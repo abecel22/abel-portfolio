efc20d015b65fd8845c7b055ab5760985c734441
abecel22 <abe2211@gmail.com>
2019-03-29 22:06:49 -0500

    Fixed img path on src.

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,13 +2,6 @@ import React from 'react'
 import { project } from '../../pages/projectsInfo'
 import Link from 'gatsby-link'
 
-import campDash from '../../assets/images/projects/campDash.png'
-import camp from '../../assets/images/projects/camp.png'
-import dashboard from './../../assets/images/projects/dashboard.png'
-import quote from './../../assets/images/projects/quote.png'
-import weather from './../../assets/images/projects/weather.png'
-import wiki from './../../assets/images/projects/wiki.png'
-
 import './style.scss'
 
 class Projects extends React.Component {
@@ -18,7 +11,10 @@ class Projects extends React.Component {
         {project.map(p => (
           <div>
             <Link to="/costa-admin/">
-              <img src={p.img} className="projects-row__image" />
+              <img
+                src={require('../../assets/images/projects/' + p.img + '.png')}
+                className="projects-row__image"
+              />
               <h4>{p.name}</h4>
               <p className="projects-row_info">{p.briefIntro}</p>
             </Link>
